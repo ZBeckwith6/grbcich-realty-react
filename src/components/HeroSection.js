c4a11e614b8f6aecb71b9fd21eaa813aea647148
Zach Beckwith <zbeckwith6@gmail.com>
2021-06-05 00:15:32 -0500

    Hero and Pages

---
+++ src/components/HeroSection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <image src="/img/contactMe.jpg" />
+            <h1>Will Grbcich Realty</h1>
+            <p>Helping St Louis Find Their Dream Homes</p>
+            <div className="hero-btns">
+                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+                    Buyers
+                </Button>
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
+                    Sellers
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
